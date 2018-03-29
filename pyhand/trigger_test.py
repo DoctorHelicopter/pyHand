3e5c1023db9e0fb99b4bec07d5b7c4507a43e55d
Sam Rubin <samuelarubin@gmail.com>
2018-03-29 17:39:27 -0500

    Reorganize

--- pyhand/trigger_test.py
+++ pyhand/trigger_test.py
@@ -5,6 +5,22 @@ import pigpio
 from picamera import PiCamera
 
 buttonPin = 14
+ 
+pi = pigpio.pi()
+if not pi.connected:
+    exit()
+
+# Setup the button
+pi.set_mode(buttonPin, pigpio.INPUT)
+pi.set_pull_up_down(buttonPin, pigpio.PUD_UP)
+pi.set_glitch_filter(buttonPin, 5000) # 5000 micros debounce
+
+cb = pi.callback(buttonPin, pigpio.EITHER_EDGE, callback)
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+
 
 def callback(gpio, level, tick):
     if level == 0: # button press
@@ -16,24 +32,6 @@ def take_picture():
     camera.capture(filename)
     print("Picture added! {}".format(filename))
 
-if __name__=="__main__":
-    global pi
-    global camera 
-     
-    pi = pigpio.pi()
-    if not pi.connected:
-        exit()
-
-    # Setup the button
-    pi.set_mode(buttonPin, pigpio.INPUT)
-    pi.set_pull_up_down(buttonPin, pigpio.PUD_UP)
-    pi.set_glitch_filter(buttonPin, 5000) # 5000 micros debounce
-
-    cb = pi.callback(buttonPin, pigpio.EITHER_EDGE, callback)
-
-    camera = PiCamera()
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-
-    while True: # all the work is done in the callback
-        time.sleep(1)
+    
+while True: # all the work is done in the callback
+    time.sleep(1)
